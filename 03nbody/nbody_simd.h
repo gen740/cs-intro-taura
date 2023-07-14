948fe64a1615760f208cbc4a445ec07a627c3682
gen740 <gen740.f@gmail.com>
2023-07-14 14:25:04 +0900

    Update

--- 03nbody/nbody_simd.h
+++ 03nbody/nbody_simd.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "particle.h"
+#include <cstdint>
 #include <iostream>
+
 /**
    @file nbody_seq.h --- single-core, non-vectorized implementation
  */
@@ -10,9 +12,9 @@
    r/sqrt(r^2+eps^2))
    @return pがqから受けるポテンシャル (-mM/(r^2+eps^2))
  */
-realv interact2(particlev *p, /** 力を受ける粒子 */
-                particle *q,  /** 力を与える粒子 */
-                real eps      /** ソフトニングパラメータ */
+inline realv interact2(particlev *p, /** 力を受ける粒子 */
+                       particle *q,  /** 力を与える粒子 */
+                       real eps      /** ソフトニングパラメータ */
 ) {
   vecv dx = q->pos - p->pos;
   realv r2 = norm2v(dx) + eps * eps;
@@ -26,16 +28,16 @@ realv interact2(particlev *p, /** 力を受ける粒子 */
    @brief 全粒子(p[0] ... p[n-1])が受ける加速度を計算
    @return 全ポテンシャル
  */
-real interact_all(long n,        /** 粒子数 */
-                  particle *p,   /** 粒子配列 */
-                  particlev *pv, /** SIMD粒子の配列(未使用)  */
-                  options_t *o   /** コマンドラインオプション  */
+inline real interact_all(long n,        /** 粒子数 */
+                         particle *p,   /** 粒子配列 */
+                         particlev *pv, /** SIMD粒子の配列(未使用)  */
+                         options_t *o /** コマンドラインオプション  */
 ) {
-  realv U = realv{0.0};
+  realv U{0.0};
   real eps = o->eps;
 
-  for (int i = 0; i < n / n_lanes; i++) {
-    for (int j = 0; j < n_lanes; j++) {
+  for (long i = 0; i < n / n_lanes; i++) {
+    for (long j = 0; j < n_lanes; j++) {
       pv[i].m[j] = p[i * n_lanes + j].m;
       pv[i].pos.x[0][j] = p[i * n_lanes + j].pos.x[0];
       pv[i].pos.x[1][j] = p[i * n_lanes + j].pos.x[1];
@@ -48,19 +50,27 @@ real interact_all(long n,        /** 粒子数 */
 
   for (long i = 0; i < n / n_lanes; i++) {
     for (long j = 0; j < n; j++) {
-      U += interact2(pv + i, p + j, eps);
+      uint64_t diff = static_cast<uint64_t>(j - i * n_lanes);
+      realv factor{0.0};
+      for (int k = 0; k < n_lanes; k++) {
+        factor[k] = 1.0;
+      }
+      if (diff < 8) {
+        factor[diff] = 0.0;
+      }
+      U += interact2(pv + i, p + j, eps) * factor;
     }
   }
 
-  for (int i = 0; i < n / n_lanes; i++) {
-    for (int j = 0; j < n_lanes; j++) {
+  for (long i = 0; i < n / n_lanes; i++) {
+    for (long j = 0; j < n_lanes; j++) {
       p[i * n_lanes + j].acc.x[0] = pv[i].acc.x[0][j];
       p[i * n_lanes + j].acc.x[1] = pv[i].acc.x[1][j];
       p[i * n_lanes + j].acc.x[2] = pv[i].acc.x[2][j];
     }
   }
 
-  real sum = 0.0;
+  real sum{0};
   for (int i = 0; i < n_lanes; i++) {
     sum += U[i];
   }
@@ -72,8 +82,8 @@ real interact_all(long n,        /** 粒子数 */
    @brief 全粒子(p[0] ... p[n-1])の運動エネルギー
    @return 全運動エネルギー
  */
-real kinetic(long n,     /** 粒子数 */
-             particle *p /** 粒子配列 */
+inline real kinetic(long n,     /** 粒子数 */
+                    particle *p /** 粒子配列 */
 ) {
   real K = 0.0;
   for (long i = 0; i < n; i++) {
@@ -87,9 +97,9 @@ real kinetic(long n,     /** 粒子数 */
    全粒子のdt時間後の速度と位置を計算
    @return dt
  */
-real update(long n,      /** 粒子数 */
-            particle *p, /** 粒子配列 */
-            real dt      /** 時間 */
+inline real update(long n,      /** 粒子数 */
+                   particle *p, /** 粒子配列 */
+                   real dt      /** 時間 */
 ) {
   for (long i = 0; i < n; i++) {
     p[i].vel = p[i].vel + p[i].acc * dt;
