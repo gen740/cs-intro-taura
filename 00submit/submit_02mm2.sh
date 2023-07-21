605822808e9705dfb231ef443157ed5cada75470
gen740 <gen740.f@gmail.com>
2023-07-21 11:43:46 +0900

    Update

--- 00submit/submit_02mm2.sh
+++ 00submit/submit_02mm2.sh
@@ -29,28 +29,28 @@ batch_main() {
     echo "hostname: $(hostname)"
 
     echo "################################################## Omp"
-    OMP_PROC_BIND=true OMP_NUM_THREAD=2 ../02mm/exe/mm_omp.exe -K 4096 -M 256 -N 4096 > mm_omp2_1.log
+    OMP_PROC_BIND=true OMP_NUM_THREAD=2 ../02mm/exe/mm_omp.exe -K 1024 -M 256 -N 1024 > mm_omp2_1.log
 
     echo "################################################## Omp"
-    OMP_PROC_BIND=true OMP_NUM_THREAD=4 ../02mm/exe/mm_omp.exe -K 4096 -M 256 -N 4096 > mm_omp4_1.log
+    OMP_PROC_BIND=true OMP_NUM_THREAD=4 ../02mm/exe/mm_omp.exe -K 1024 -M 256 -N 1024 > mm_omp4_1.log
 
     echo "################################################## Omp"
-    OMP_PROC_BIND=true OMP_NUM_THREAD=8 ../02mm/exe/mm_omp.exe -K 4096 -M 256 -N 4096 > mm_omp8_1.log
+    OMP_PROC_BIND=true OMP_NUM_THREAD=8 ../02mm/exe/mm_omp.exe -K 1024 -M 256 -N 1024 > mm_omp8_1.log
 
     echo "################################################## Omp"
-    OMP_PROC_BIND=true OMP_NUM_THREAD=16 ../02mm/exe/mm_omp.exe -K 4096 -M 256 -N 4096 > mm_omp16_1.log
+    OMP_PROC_BIND=true OMP_NUM_THREAD=16 ../02mm/exe/mm_omp.exe -K 1024 -M 256 -N 1024 > mm_omp16_1.log
 
     echo "################################################## Omp"
-    OMP_PROC_BIND=true OMP_NUM_THREAD=32 ../02mm/exe/mm_omp.exe -K 4096 -M 256 -N 4096 > mm_omp32_1.log
+    OMP_PROC_BIND=true OMP_NUM_THREAD=32 ../02mm/exe/mm_omp.exe -K 1024 -M 256 -N 1024 > mm_omp32_1.log
 
     echo "################################################## Omp"
-    OMP_PROC_BIND=true OMP_NUM_THREAD=56 ../02mm/exe/mm_omp.exe -K 4096 -M 256 -N 4096 > mm_omp56_1.log
+    OMP_PROC_BIND=true OMP_NUM_THREAD=56 ../02mm/exe/mm_omp.exe -K 1024 -M 256 -N 1024 > mm_omp56_1.log
 
     echo "################################################## SIMD"
-    ../02mm/exe/mm_simd.exe -K 4096 -M 256 -N 4096 > mm_simd_1.log
+    ../02mm/exe/mm_simd.exe -K 1024 -M 256 -N 1024 > mm_simd_1.log
 
     echo "################################################## Sequencial"
-    ../02mm/exe/mm_seq.exe -K 4096 -M 256 -N 4096 > mm_seq_1.log
+    ../02mm/exe/mm_seq.exe -K 1024 -M 256 -N 1024 > mm_seq_1.log
 }
 
 # ここから下は書き換える必要なし
